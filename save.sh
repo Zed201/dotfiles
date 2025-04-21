7fcc52ee0665c868270d00dcffe05a9e53b9203e
Luiz Gustavo <luizg0509@gmail.com>
2025-04-21 15:06:44 -0300

    Add yazi

--- save.sh
+++ save.sh
@@ -2,9 +2,9 @@
 
 # Array de pastas a serem backupadas
 PASTAS=("anyrun" "code-flags.conf" "hypr" "nvim" "waybar"
-  "wireplumber" "wofi" "clipse" "htop" "mako" "scripts" "wlogout")
+        "wireplumber" "wofi" "clipse" "htop" "mako" "scripts" "wlogout" "yazi")
 
 # Copiar cada pasta para a pasta de destino
 for pasta in "${PASTAS[@]}"; do
-  cp -r "$HOME/.config/$pasta" ".config/"
+        cp -r "$HOME/.config/$pasta" ".config/"
 done
